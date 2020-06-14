24119f6be4b9e4fc33996df9a6b2f34017659856
Kimberley Mackenzie <kimberley@textio.com>
2020-06-14 14:45:31 -0700

    Implement insert and float.

--- src/data_structures/heap.js
+++ src/data_structures/heap.js
@@ -3,15 +3,15 @@ class MaxHeap {
 
   /**
    * Create a new empty max heap of a given size, optionally from an existing array
-   * 
+   *
    * @param {number} [size=1023] Maximum capacity of the queue
    * @param {{priority: number, element: *}[]} [fromArray] Build the heap from this array instead. The given array must be 1-indexed, and records must have the given form.
    */
   constructor({ size = this.constructor.DEFAULT_SIZE, fromArray } = {}) {
     if (fromArray) {
       this._storage = fromArray;
-      this._size = fromArray.length - 1;
-      this._count = this._size;
+      this.size = fromArray.length - 1;
+      this._count = this.size;
       this._buildheap();
 
     } else {
@@ -32,7 +32,7 @@ class MaxHeap {
 
   /**
    * Use a heap to sort an array in-place in n*log(n) time
-   * 
+   *
    * @param {{priority: number, element: *}[]} [array] Data to sort. The given array must be 1-indexed, and records must have the given form.
    */
   static heapsort(array) {
@@ -52,6 +52,10 @@ class MaxHeap {
     return Math.floor(i / 2);
   }
 
+  _priority(i) {
+    return this._storage[i].priority;
+  }
+
   _swap(i, j) {
     // Note: in a language like C, Java or Rust, where the array is full of records
     // instead of references to records, we would need to swap the priority and
@@ -62,7 +66,13 @@ class MaxHeap {
   }
 
   _float(i) {
-    // TODO
+    let parent = this._parent(i);
+    let node = i;
+    while(parent > 0 && this._priority(parent) > this._priority(node)) {
+      this._swap(node, parent);
+      node = parent;
+      parent = this._parent(node);
+    }
   }
 
   _sink(i) {
@@ -75,27 +85,37 @@ class MaxHeap {
 
   /**
    * Add a record to the queue with a given priority
-   * 
+   *
    * @param {number} priority Priority of the record
    * @param {*} element Data to store in this record
    * @throws If the heap is full
    */
   insert(priority, element) {
-    // TODO
+    if (this._count === this.size){
+      throw new Error("No more space left!")
+    };
+
+    this._count += 1;
+
+    const newRecord = this._storage[this._count];
+    newRecord.priority = priority;
+    newRecord.element = element;
+
+    this._float(this._count);
   }
 
   /**
    * Remove and return the record with the highest priority
-   * 
+   *
    * @returns {*} The data stored in the highest-priority record, or undefined if the queue is empty
    */
   removeMax() {
     // TODO
   }
 
-  /** 
+  /**
    * How many records are in the priority queue?
-   * 
+   *
    * @returns {number} Record count
    */
   count() {
@@ -104,10 +124,10 @@ class MaxHeap {
 
   /**
    * Turn this max heap into a sorted array
-   * 
+   *
    * Destroys the max heap in the process - insert, removeMax, etc will NOT
    * work after this function has been run
-   * 
+   *
    * @returns Sorted storage array. Note that the array is 1-indexed (so the first element is null)
    */
   sort() {
